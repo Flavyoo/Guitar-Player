e98aff4cdb6dcef1be8846f848efbabd3c96e244
Flavyoo <flavioandrade1918@gmail.com>
2017-03-17 03:23:03 -0400

    Updated Keyboard
    Updated Keyboard to look more like a piano.

--- GuitarHero.java
+++ GuitarHero.java
@@ -8,8 +8,8 @@ public class GuitarHero {
 	public static int SAMPLE_RATE = 44100;
 	public static double sample = 0;
 	public static int j = 0;
-	public static long start = System.currentTimeMillis();
-	public static double KEYBOARD_REFRESH_DELAY = 0.001;
+	public static long start = 0;
+	public static double KEYBOARD_REFRESH_DELAY = 0.1;
 	public static String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
 	public static void black_key(int x, int y, boolean pressed) {
@@ -30,6 +30,7 @@ public class GuitarHero {
 	}
 
     public static void drawKeyboard(int pressed) {
+<<<<<<< Updated upstream
             StdDraw.clear();
 	    white_key(0, 0, pressed == 0);
 	    white_key(48, 0, pressed == 2);
@@ -68,6 +69,46 @@ public class GuitarHero {
 	    black_key(852, 130, pressed == 30);
 	    black_key(948, 130, pressed == 33);
 	    black_key(996, 130, pressed == 35);
+=======
+		StdDraw.clear();
+		white_key(48, 0, pressed == 0);
+	    white_key(96, 0, pressed == 2);
+	    white_key(144, 0, pressed == 3);
+	    white_key(192, 0, pressed == 5);
+	    white_key(240, 0, pressed == 7);
+	    white_key(288, 0, pressed == 8);
+	    white_key(336, 0, pressed == 10);
+	    white_key(384, 0, pressed == 12);
+	    white_key(432, 0, pressed == 14);
+	    white_key(480, 0, pressed == 15);
+	    white_key(528, 0, pressed == 17);
+	    white_key(576, 0, pressed == 19);
+	    white_key(624, 0, pressed == 20);
+	    white_key(672, 0, pressed == 22);
+	    white_key(720, 0, pressed == 24);
+	    white_key(768, 0, pressed == 26);
+	    white_key(816, 0, pressed == 27);
+	    white_key(864, 0, pressed == 29);
+	    white_key(912, 0, pressed == 31);
+	    white_key(960, 0, pressed == 32);
+	    white_key(1008, 0, pressed == 34);
+	    white_key(1056, 0, pressed == 36);
+	    black_key(48, 270, pressed == 1);
+	    black_key(144, 270, pressed == 4);
+	    black_key(192, 270, pressed == 6);
+	    black_key(288, 270, pressed == 9);
+	    black_key(336, 270, pressed == 11);
+	    black_key(384, 270, pressed == 13);
+	    black_key(480, 270, pressed == 16);
+	    black_key(528, 270, pressed == 18);
+	    black_key(624, 270, pressed == 21);
+	    black_key(672, 270, pressed == 23);
+	    black_key(720, 270, pressed == 25);
+	    black_key(814, 270, pressed == 28);
+	    black_key(864, 270, pressed == 30);
+	    black_key(960, 270, pressed == 33);
+	    black_key(1008, 270, pressed == 35);
+>>>>>>> Stashed changes
 	}
 
 	// Add a tone effect to the sample.
@@ -81,15 +122,13 @@ public class GuitarHero {
 	    StdDraw.setXscale(0, 1056);
 	    StdDraw.setYscale(0, 300);
 
-
+        // Creates 37 Notes
 		GuitarString[] keys = new GuitarString[37];
 		for (int i = 0; i < 37; i++) {
 			double CONCERT_I = 440 * Math.pow(1.05956, i - 24);
 			keys[i] = new GuitarString(CONCERT_I);
 		}
 
-		GuitarHeroVisualizer ghv = new GuitarHeroVisualizer();
-		//ghv.initialize(500, 500, -1, -5, 500, 5);
 		while (true) {
 			if (StdDraw.hasNextKeyTyped()) {
 				char key = StdDraw.nextKeyTyped();
@@ -112,9 +151,7 @@ public class GuitarHero {
 			StdAudio.play(a);
 			keys[j].tic();
 			keys[(j + 1) % keys.length].tic();
-			//ghv.plot(sample, keys[j].time());
-
-			// Refresh the keyboard.
+			// Refresh the keyboard. / Keyboard does not update on time with key pressed.
 			start = System.currentTimeMillis();
 			if (System.currentTimeMillis() - start > KEYBOARD_REFRESH_DELAY) {
 				start = System.currentTimeMillis();
